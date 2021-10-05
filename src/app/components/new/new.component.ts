3c2243c5c7ccf2646a86e4861e61f469cb95d95b
Raúl Sulca Palacios <raul_97_02@hotmail.com>
2021-10-04 19:09:55 -0500

    app

--- src/app/components/new/new.component.ts
+++ src/app/components/new/new.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 import { Articles, Article } from '../../interfaces/article';
 
 @Component({
@@ -8,13 +9,44 @@ import { Articles, Article } from '../../interfaces/article';
 })
 export class NewComponent implements OnInit {
 
-  @Input("noticia") n:Article;
-  @Input("index") i:number;
+  @Input("noticia") n: Article;
+  @Input("index") i: number;
 
-  constructor() {
-  
+  constructor(public actionSheetController: ActionSheetController) {
+
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  async lanzarMenu() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Opciones',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Compartir',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Favorito',
+        icon: 'star',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
 
 }
